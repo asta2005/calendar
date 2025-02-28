8e042d7e712ae67457138f030fbae9c8b4a2a0c7
Asta <omaralinora.1@gmail.com>
2025-02-28 11:35:29 +0100

    dag 1

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+require 'database.php';
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalender App</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="script.js"></script>
+</head>
+<body>
+    <h1>Kalender</h1>
+    <div id="calendar"></div>
+</body>
+</html>
