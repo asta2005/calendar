39e803071154a66f3bf199f9b3155daf07ab9559
Asta <omaralinora.1@gmail.com>
2025-02-28 12:41:55 +0100

    test kal

--- public/index.php
+++ public/index.php
@@ -1,17 +1,45 @@
 <?php
-require 'database.php';
+include 'templates/header.php';
+include 'templates/navbar.php';
+include 'includes/functions.php';
 ?>
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalender App</title>
-    <link rel="stylesheet" href="style.css">
-    <script defer src="script.js"></script>
-</head>
-<body>
-    <h1>Kalender</h1>
+
+<div class="container mt-5">
+    <h1>Event Calendar</h1>
     <div id="calendar"></div>
-</body>
-</html>
+</div>
+
+<!-- Modal for adding/editing events -->
+<div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="eventModalLabel">Add/Edit Event</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="eventForm">
+                    <input type="hidden" id="eventId">
+                    <div class="form-group">
+                        <label for="eventTitle">Event Title</label>
+                        <input type="text" class="form-control" id="eventTitle" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="startTime">Start Time</label>
+                        <input type="text" class="form-control datetimepicker" id="startTime" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="endTime">End Time</label>
+                        <input type="text" class="form-control datetimepicker" id="endTime" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Save Event</button>
+                    <button type="button" class="btn btn-danger" id="deleteEvent">Delete Event</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'templates/footer.php'; ?>
